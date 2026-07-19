341bb744d13d952eda7bb57f94c4a93dba79ec3c
Balaji V <balajiv@Balajis-MacBook-Air.local>
2026-07-19 23:12:20 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { DeskFolio } from './components/DeskFolio.jsx'
+import 'deskfolio/styles.css'
+import './index.css'
+
+function App() {
+  return <DeskFolio />
+}
+
+export default App
---
+++ src/components/DeskFolio.d.ts
@@ -0,0 +1,3 @@
+import { ReactElement } from 'react';
+export declare function DeskFolio(): ReactElement;
+export declare function DeskFolioPage(): ReactElement;
